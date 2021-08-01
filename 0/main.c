c3420690014bd8a9f700000185db1767821838b3
bad5ect0r <bad5ect0r@pm.me>
2021-08-01 10:36:47 -0400

    Removing a.outs and making 0/main.c print malloced addresses

--- 0/main.c
+++ 0/main.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main() {
+	setvbuf(stdout, NULL, 0x2, 0);
+
 	void *a = malloc(0x28);
+	printf("void *a = %p\n", a);
 	void *b = malloc(0x38);
+	printf("void *b = %p\n", b);
 	void *c = malloc(0x48);
+	printf("void *c = %p\n", c);
 	void *d = malloc(0x58);
+	printf("void *d = %p\n", d);
 
 	free(a);
 	free(b);
